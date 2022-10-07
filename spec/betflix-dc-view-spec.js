353f62e64457db123560ba2ad75009ebf416a09d
Pobthorn <agonlive@gmail.com>
2022-10-07 20:32:31 +0700

    ok

---
+++ spec/betflix-dc-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BetflixDcView from '../lib/betflix-dc-view';
+
+describe('BetflixDcView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
